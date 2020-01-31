f417a1f101a961d6eb01d983b79c1b4eadf2b68a
Hailey Bongo <haileyebongo@gmail.com>
2020-01-31 19:54:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarf_names)
-    if block_given?
+    if block_given? == true
      i = 0
      while i < dwarf_names.length
       dwarf_names.each_with_index { |item, index| puts "#{index}:#{item}" }
