ba0ca80d1f6964ec7895d424f308a48ab0473a30
Hailey Bongo <haileyebongo@gmail.com>
2020-02-01 00:48:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect {|food| food == cheese_types[0] || food == cheese_types [1] || food == cheese_types [2] || food == cheese_types [3]
+  array.detect {|food| food == cheese_types[0] || food == cheese_types [1] || food == cheese_types [2] || food == cheese_types [3]}
   end
 end
