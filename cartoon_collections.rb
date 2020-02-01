53f892d04df3f48216733c240047c1c9f086388f
Hailey Bongo <haileyebongo@gmail.com>
2020-02-01 00:41:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.
+  array.any? { |word| word.length > 4}
 end
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
